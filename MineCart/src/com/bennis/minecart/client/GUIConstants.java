9b7c7284195aee4e3ddbc9a9eaf707f14f9acd98
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-02-29 15:45:58 +0000

    Added Scroll speeds to UI Constants.

--- MineCart/src/com/bennis/minecart/client/GUIConstants.java
+++ MineCart/src/com/bennis/minecart/client/GUIConstants.java
@@ -6,5 +6,8 @@ public interface GUIConstants
 	public static final int HEIGHT = 600;
 	public static final String CONTAINER_NAME = "canvasContainer";
 	public static final String BLACK = "black";
+	public static final int SLOW_SCROLL_SPEED = 1;
+	public static final int MEDIUM_SCROLL_SPEED = 3;
+	public static final int FAST_SCROLL_SPEED = 4;
 }
 
