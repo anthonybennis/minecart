2f466d08240c96e5c48671c49bd2b7d549bbeb95
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-01 08:24:27 +0000

    Temp keeping railway flat....

--- trunk/MineCart/src/com/bennis/minecart/client/RailwayTrack.java
+++ trunk/MineCart/src/com/bennis/minecart/client/RailwayTrack.java
@@ -16,20 +16,29 @@ public class RailwayTrack extends Platform
 	@Override
 	public List<Line> createLineSegments() 
 	{
+		/*
+		 * We'll keep a basic flat platform for v 0.1
+		 */
 		List<Line> lineSegments = new ArrayList<Line>();
 		int x = 0;
-		int x1 = 600;
 		int y = 500;
+		int x1 = GUIConstants.WIDTH*2;
 		int y1 = y;
-		lineSegments.add(new Line(x, y, x1, y1)); // Line 1
-		x = x1; y1= y1-50;x1= x1+1000;
-		lineSegments.add(new Line(x, y, x1, y1)); // Line 2
-		x = x1; x1= x1+100; y = y1;
-		lineSegments.add(new Line(x, y, x1, y1)); // Line 3
-		x = x1;  y=y1; y1= y1+50;x1= x1+300;
-		lineSegments.add(new Line(x, y, x1, y1)); // Line 4
-		x = x1;  y=y1;x1= x1+100;
-		lineSegments.add(new Line(x, y, x1, y1)); // Line 5
+		lineSegments.add(new Line(x, y, x1, y1)); 
+		
+		
+		/*
+		 * Example of how you'd create a bump on the track
+		 */
+//		lineSegments.add(new Line(x, y, x1, y1)); // Line 1
+//		x = x1; y1= y1-50;x1= x1+1000;
+//		lineSegments.add(new Line(x, y, x1, y1)); // Line 2
+//		x = x1; x1= x1+100; y = y1;
+//		lineSegments.add(new Line(x, y, x1, y1)); // Line 3
+//		x = x1;  y=y1; y1= y1+50;x1= x1+300;
+//		lineSegments.add(new Line(x, y, x1, y1)); // Line 4
+//		x = x1;  y=y1;x1= x1+100;
+//		lineSegments.add(new Line(x, y, x1, y1)); // Line 5
 		
 		return lineSegments;
 	}
