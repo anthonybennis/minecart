24fa1fb2fd5c4f7bf94924be0c91bc71b1b541be
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-01 09:49:31 +0000

    Added GWT Voices library.

--- trunk/MineCart/src/com/bennis/minecart/client/MineCartEntryPoint.java
+++ trunk/MineCart/src/com/bennis/minecart/client/MineCartEntryPoint.java
@@ -1,6 +1,7 @@
 package com.bennis.minecart.client;
 
 import com.bennis.minecart.client.engine.logic.AGame;
+import com.bennis.minecart.client.engine.logic.Playlist;
 import com.bennis.minecart.client.engine.model.Scene;
 import com.bennis.minecart.client.engine.ui.AGameEntryPoint;
 import com.google.gwt.canvas.client.Canvas;
@@ -9,13 +10,12 @@ import com.google.gwt.canvas.client.Canvas;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class MineCartEntryPoint extends AGameEntryPoint
-{	
-	
-
+{
 	@Override
 	public AGame createGame(Canvas bufferCanvas, Canvas canvas, Scene scene) 
 	{
-		AGame game = new MineCartGame(bufferCanvas, canvas, scene, this.getHTMLContainerName());
+		Playlist playList = new MineCartPlayList();
+		AGame game = new MineCartGame(bufferCanvas, canvas, scene, this.getHTMLContainerName(),playList);
 		return game;
 	}
 	
