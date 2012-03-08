46ebe76234d6477de659894813f6ee9319a76d4f
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-08 23:08:07 +0000

    Added wrapper class for Enyo

---
+++ MineCart/src/com/bennis/minecart/client/engine/logic/Enyo.java
@@ -0,0 +1,14 @@
+package com.bennis.minecart.client.engine.logic;
+
+/**
+ * JSNI Wrapper for Enyo functionality.
+ * @author Anthony
+ */
+public class Enyo 
+{
+
+	/*
+	 * TODO AB
+	 * Do we need any enyo? IO?
+	 */
+}
