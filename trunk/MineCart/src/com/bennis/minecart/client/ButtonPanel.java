6c54a0d1d1415148a2f7aa0d1e44bd253609e4c1
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-11 21:54:38 +0000

    Fixed layout

--- trunk/MineCart/src/com/bennis/minecart/client/ButtonPanel.java
+++ trunk/MineCart/src/com/bennis/minecart/client/ButtonPanel.java
@@ -5,8 +5,8 @@ import com.bennis.minecart.client.engine.logic.InputEvent;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -21,84 +21,55 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class ButtonPanel 
 {
 	private enum BUTTON_TYPE{LEFT, LEFT_JUMP,RIGHT, RIGHT_JUMP, PAUSE, EXIT};
-	private final String BUTTON_WIDTH = "0px"; 
-	private final String BUTTON_HEIGHT = "0px";
+	private final String BUTTON_WIDTH = "100px"; 
 	private final AGame _game;
 	
 	public ButtonPanel(AGame game)
 	{
 		_game = game;
 		
-		AbsolutePanel flowPanel = new AbsolutePanel();
+		HorizontalPanel flowPanel = new HorizontalPanel();
 		RootPanel.get(GUIConstants.BUTTON_CONTAINER_NAME).add(flowPanel);
 		flowPanel.setSize(GUIConstants.WIDTH_PX, "144px");
-		
+		Resources resources = GWT.create(Resources.class);
 		/*
 		 * Create LEFT button
 		 */
-		Resources resources = GWT.create(Resources.class);
-		Image image = new Image(resources.leftButton());
-		PushButton leftButton = new PushButton(image);
-		leftButton.setWidth(BUTTON_WIDTH);
-		leftButton.setHeight(BUTTON_HEIGHT);
-		leftButton.addClickHandler(new MineCartButtonListener(BUTTON_TYPE.LEFT));
-		int x = 0;
-		flowPanel.add(leftButton, x, 0);
+		this.createButton(resources.leftButton(), BUTTON_TYPE.LEFT, flowPanel);
 		
 		/*
 		 * Create LEFT Jump button
 		 */
-		image = new Image(resources.jumpButton());
-		PushButton leftJumpButton = new PushButton(image);
-		leftJumpButton.addClickHandler(new MineCartButtonListener(BUTTON_TYPE.LEFT_JUMP));
-		leftJumpButton.setWidth(BUTTON_WIDTH);
-		leftJumpButton.setHeight(BUTTON_HEIGHT);
-		x = x + 162;
-		flowPanel.add(leftJumpButton, x, 0);
+		this.createButton(resources.jumpButton(), BUTTON_TYPE.LEFT_JUMP, flowPanel);
 		
 		/*
 		 * Create Pause Button
 		 */
-		image = new Image(resources.pauseButton());
-		PushButton pauseButton = new PushButton(image);
-		pauseButton.addClickHandler(new MineCartButtonListener(BUTTON_TYPE.RIGHT));
-		pauseButton.setWidth(BUTTON_WIDTH);
-		pauseButton.setHeight(BUTTON_HEIGHT);
-		x = x + 162;
-		flowPanel.add(pauseButton, x, 0);
+		this.createButton(resources.pauseButton(), BUTTON_TYPE.PAUSE, flowPanel);
 		
 		/*
 		 * Create Exit button
 		 */
-		image = new Image(resources.pauseButton());
-		PushButton exitButton = new PushButton(image); 
-		exitButton.addClickHandler(new MineCartButtonListener(BUTTON_TYPE.RIGHT));
-		exitButton.setWidth(BUTTON_WIDTH);
-		exitButton.setHeight(BUTTON_HEIGHT);
-		x = x + 162;
-		flowPanel.add(exitButton, x, 0);
+		this.createButton(resources.pauseButton(), BUTTON_TYPE.EXIT, flowPanel);
 		
 		/*
 		 * Create RIGHT JUMP button
 		 */
-		image = new Image(resources.jumpButton());
-		PushButton rightJumpButton = new PushButton(image); 
-		rightJumpButton.addClickHandler(new MineCartButtonListener(BUTTON_TYPE.RIGHT));
-		rightJumpButton.setWidth(BUTTON_WIDTH);
-		rightJumpButton.setHeight(BUTTON_HEIGHT);
-		x = x + 162;
-		flowPanel.add(rightJumpButton, x, 0);
+		this.createButton(resources.jumpButton(), BUTTON_TYPE.RIGHT_JUMP, flowPanel);
 		
 		/*
 		 * Create RIGHT button
 		 */	
-		image = new Image(resources.rightButton());
-		PushButton rightButton = new PushButton(image);
-		rightButton.addClickHandler(new MineCartButtonListener(BUTTON_TYPE.RIGHT));
-		rightButton.setWidth(BUTTON_WIDTH);
-		rightButton.setHeight(BUTTON_HEIGHT);
-		x = x + 162;
-		flowPanel.add(rightButton, x, 0);
+		this.createButton(resources.rightButton(), BUTTON_TYPE.RIGHT, flowPanel);
+	}
+	
+	private void createButton(ImageResource imageResource, BUTTON_TYPE type, HorizontalPanel panel)
+	{
+		Image image = new Image(imageResource);
+		PushButton button = new PushButton(image);
+		button.setWidth(BUTTON_WIDTH);
+		button.addClickHandler(new MineCartButtonListener(type));
+		panel.add(button); 
 	}
 	
 	class MineCartButtonListener implements ClickHandler
