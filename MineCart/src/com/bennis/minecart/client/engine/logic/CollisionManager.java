5efe843b5ecef40ce2a027a5ae9d63024894f531
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-13 11:53:01 +0000

    New collision manager class. Helps detects and deletegates collisions in an optimal manner.

--- MineCart/src/com/bennis/minecart/client/engine/logic/CollisionManager.java
+++ MineCart/src/com/bennis/minecart/client/engine/logic/CollisionManager.java
@@ -1,6 +1,80 @@
 package com.bennis.minecart.client.engine.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bennis.minecart.client.engine.model.ISprite;
+import com.bennis.minecart.client.engine.model.ISprite.Type;
+import com.bennis.minecart.client.engine.model.Scene;
+
+/**
+ * 
+ * @author abennis
+ */
 public class CollisionManager 
 {
-
+	/**
+	 * 
+	 * @param scene
+	 */
+	public void handleCollisions(Scene scene)
+	{
+		List<ISprite> sprites = this.getCollidableSprites(scene);
+		/*
+		 * Check every sprite against every other sprite to see if they
+		 * collide.
+		 */
+		for (ISprite iSprite : sprites) 
+		{
+			for (ISprite sprite : sprites) 
+			{
+				if (iSprite != sprite)
+				{
+					if (iSprite.doSpritesCollide(sprite))
+					{
+						iSprite.handleCollision(sprite);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Only certain layers, and certain types of Sprites are
+	 * capabile of colliding. This method puts them all in
+	 * one List.
+	 * Note: I'm returning ArrayList as opposed to List, as this
+	 * is better for performance with GWT.
+	 * 
+	 * @param scene
+	 * @return a List of all Sprites that can collide.
+	 */
+	private ArrayList<ISprite> getCollidableSprites(Scene scene)
+	{
+		ArrayList<ISprite> collidableSpritesList = new ArrayList<ISprite>();
+		
+		collidableSpritesList = this.addCollidableSprites(collidableSpritesList, scene.getBackLayer());
+		collidableSpritesList = this.addCollidableSprites(collidableSpritesList, scene.getMiddleLayer());
+		collidableSpritesList = this.addCollidableSprites(collidableSpritesList, scene.getFrontLayer());
+		
+		return collidableSpritesList;
+	}
+	
+	/**
+	 * Creates a sub list of sprites that can collide from source list.
+	 * @param listOfSprites
+	 * @return
+	 */
+	private ArrayList<ISprite> addCollidableSprites(ArrayList<ISprite> collidableSpritesList, List<ISprite> listOfSprites)
+	{
+		for (ISprite iSprite : listOfSprites) 
+		{
+			if (iSprite.getType() != Type.DECORATION)
+			{
+				collidableSpritesList.add(iSprite);
+			}
+		}
+		
+		return collidableSpritesList;
+	}
 }
