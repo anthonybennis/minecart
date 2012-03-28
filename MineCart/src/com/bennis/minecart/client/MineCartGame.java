5f24f193fc1c118afce62025258eff498a82e5f3
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-28 14:21:24 +0000

    Introduced game counters.

--- MineCart/src/com/bennis/minecart/client/MineCartGame.java
+++ MineCart/src/com/bennis/minecart/client/MineCartGame.java
@@ -3,6 +3,7 @@ package com.bennis.minecart.client;
 import com.bennis.minecart.client.engine.logic.AGame;
 import com.bennis.minecart.client.engine.logic.Playlist;
 import com.bennis.minecart.client.engine.logic.SpriteFactory;
+import com.bennis.minecart.client.engine.model.GamePointCounterSprite;
 import com.bennis.minecart.client.engine.model.Scene;
 import com.google.gwt.canvas.client.Canvas;
 
@@ -14,6 +15,8 @@ import com.google.gwt.canvas.client.Canvas;
 public class MineCartGame extends AGame 
 {
 	private String _containerName;
+	private GamePointCounterSprite _livesCounter;
+	private GamePointCounterSprite _scoreCounter;
 	
 	public MineCartGame(Canvas bufferCanvas, Canvas canvas,Scene scene, String containerName, Playlist playlist)	
 	{
@@ -30,5 +33,24 @@ public class MineCartGame extends AGame
 	public String getContainerName()
 	{
 		return _containerName;
+	}
+
+	/**
+	 * Create all the counters needed
+	 * @return
+	 */
+	@Override
+	protected GamePointCounterSprite[] createGamePointerSprites() 
+	{
+		final int counterRow = 30;
+		
+		GamePointCounterSprite[] counters = new GamePointCounterSprite[2];
+		_scoreCounter = new GamePointCounterSprite(GUIConstants.GAME_POINTS_COUNTER_NAME, 10, counterRow);
+		_livesCounter = new GamePointCounterSprite(GUIConstants.GAME_LIVES_COUNTER_NAME, (GUIConstants.WIDTH - 100), counterRow);
+		_livesCounter.incrementValue(GUIConstants.DEFAULT_NUMBER_OF_LIVES);
+		counters[0] = _scoreCounter;
+		counters[1] = _livesCounter;
+		
+		return counters;
 	}
 }
