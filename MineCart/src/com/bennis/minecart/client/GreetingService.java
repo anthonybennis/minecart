259cf7d45e4b68070218c803ab25fdc6727e52c6
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-03-30 09:02:30 +0000

    Introduced MineCart Rotation, and copyright doc.

--- MineCart/src/com/bennis/minecart/client/GreetingService.java
+++ MineCart/src/com/bennis/minecart/client/GreetingService.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Anthony Bennis
+ * All rights reserved. This program and the accompanying materials
+ * are fully owned by Anthony Bennis and cannot be used for commercial projects without prior permission from same.
+ * http://www.anthonybennis.com
+ *
+ * Contributors:
+ * Anthony Bennis.
+ *******************************************************************************/
 package com.bennis.minecart.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
