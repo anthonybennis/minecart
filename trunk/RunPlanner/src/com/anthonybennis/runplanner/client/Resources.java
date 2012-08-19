dc812574e0e8c186ebf878662b426c92c41150d7
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-08-19 11:16:33 +0000

    We now use a ClientBundle to load all images.

---
+++ trunk/RunPlanner/src/com/anthonybennis/runplanner/client/Resources.java
@@ -0,0 +1,40 @@
+package com.anthonybennis.runplanner.client;
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * API for all images used by Mine Cart.
+ * @author Anthony
+ *
+ */
+public interface Resources extends ClientBundle
+{
+	public static final Resources INSTANCE =  GWT.create(Resources.class);
+	
+	  @Source("images/Plus.png")
+	  ImageResource getPlusImage();
+	  
+	  @Source("images/Minus.png")
+	  ImageResource getMinusImage();
+	  
+	  @Source("images/CreatePlan.png")
+	  ImageResource getCreatePlanButtonImage();
+	  
+	  @Source("images/CreatePlanDown.png")
+	  ImageResource getCreatePlanDownButtonImage();
+
+	  @Source("images/DateViewBackground.png")
+	  ImageResource getDatePanelBackgroundImage();	  
+	  
+	  @Source("images/Settings.png")
+	  ImageResource getSettingsImage();	 
+	  
+	  @Source("images/RunPlannerGold.png")
+	  ImageResource getTitleLogoImage();	 
+	  
+	  @Source("images/Close.png")
+	  ImageResource getCloseAppButtonImage();
+}
