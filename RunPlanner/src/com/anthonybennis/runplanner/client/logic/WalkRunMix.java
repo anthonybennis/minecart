f809834447bb1baead5ef3e3ac127a831296e3f2
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-08-30 15:31:08 +0000

    Implememented logic of class...

--- RunPlanner/src/com/anthonybennis/runplanner/client/logic/WalkRunMix.java
+++ RunPlanner/src/com/anthonybennis/runplanner/client/logic/WalkRunMix.java
@@ -1,29 +1,24 @@
 package com.anthonybennis.runplanner.client.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TODO Add JavaDoc
- * 
- * TODO Need object for Walk, Run, SWIM/CYCLE Mix
  * 
  * @author abennis
  */
 public class WalkRunMix
 {
+	private final static String WALK_RUN_SEPERATOR = "%";
+	private List<Activity> _activeityList = new ArrayList<Activity>();
+	
 	/**
-	 * TODO AB - Implement WalkRunMix
 	 * Empty contructor, nothing to do.
 	 */
 	public WalkRunMix()
 	{
 		
-	}
-	
-	public WalkRunMix(String walkRunAsString)
-	{
-		/*
-		 * TODO Complete this.
-		 */
-		WalkRunMix.convertStringToWalkRunMix(walkRunAsString);
 	}
 	
 	/**
@@ -32,7 +27,8 @@ public class WalkRunMix
 	 */
 	public WalkRunMix(double run)
 	{
-		
+		Activity runActivity = new Activity(1, Activity.RUN, String.valueOf(run));
+		_activeityList.add(runActivity);
 	}
 	
 	/**
@@ -43,7 +39,13 @@ public class WalkRunMix
 	 */
 	public WalkRunMix(double walk, double run, double walk2)
 	{
+		Activity walk1Activity = new Activity(1,Activity.WALK, String.valueOf(walk));
+		Activity runActivity = new Activity(2,Activity.RUN, String.valueOf(run));
+		Activity walk2Activity = new Activity(3,Activity.WALK, String.valueOf(walk2));
 		
+		_activeityList.add(walk1Activity);
+		_activeityList.add(runActivity);
+		_activeityList.add(walk2Activity);
 	}
 	
 	/**
@@ -53,9 +55,24 @@ public class WalkRunMix
 	 * @param walk2
 	 * @param run2
 	 */
-	public WalkRunMix(double walk, double run, double walk2, double run2)
+	public WalkRunMix(double walk, double run, double walk2, double run2, double walk3)
 	{
+		Activity walkActivity = new Activity(1,Activity.WALK, String.valueOf(walk));
+		Activity runActivity = new Activity(2,Activity.RUN, String.valueOf(run));
+		Activity walk2Activity = new Activity(3,Activity.WALK, String.valueOf(walk2));
+		Activity run2Activity = new Activity(4,Activity.RUN, String.valueOf(run2));
+		Activity walk3Activity = new Activity(5,Activity.WALK, String.valueOf(walk3));
 		
+		_activeityList.add(walkActivity);
+		_activeityList.add(runActivity);
+		_activeityList.add(walk2Activity);
+		_activeityList.add(run2Activity);
+		_activeityList.add(walk3Activity);
+	}
+	
+	public void addActivity(Activity activity)
+	{
+		_activeityList.add(activity);
 	}
 	
 	/**
@@ -66,22 +83,36 @@ public class WalkRunMix
 	public static WalkRunMix convertStringToWalkRunMix(String walkRunMixAsString)
 	{
 		WalkRunMix mix = new WalkRunMix();
+		
+		String[] walkRunSplit = walkRunMixAsString.split(WALK_RUN_SEPERATOR);
+		Activity activity;
+		for (String string : walkRunSplit) 
+		{
+			if (!string.equals(""))
+			{
+				System.out.println("Split: " + string);
+				activity = Activity.createActivityFromString(string);
+				mix.addActivity(activity);
+			}
+		}
+		
+		
 		return mix;
 	}
 	
 	/**
 	 * 
-	 * @param walkRunMix
-	 * @return
 	 */
-	public static String convertWalkRunMixToString(WalkRunMix walkRunMix)
-	{
-		String mixAsAString = "";
-		return mixAsAString;
-	}
-	
 	public String toString()
 	{
-		return WalkRunMix.convertWalkRunMixToString(this);
+		StringBuilder walkRunAsString = new StringBuilder();
+		
+		for (Activity activity: _activeityList) 
+		{
+			walkRunAsString.append(activity.toString());
+			walkRunAsString.append(WALK_RUN_SEPERATOR);
+		}
+		
+		return walkRunAsString.toString();
 	}
 }
