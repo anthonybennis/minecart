2fcfc97fa20fa4b1d9b917c18828bf8d43dbca29
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-09-04 15:33:19 +0000

    Handler, which creates the actual Plan.

--- trunk/RunPlanner/src/com/anthonybennis/runplanner/client/handlers/CreatePlanClickHandler.java
+++ trunk/RunPlanner/src/com/anthonybennis/runplanner/client/handlers/CreatePlanClickHandler.java
@@ -1,5 +1,12 @@
 package com.anthonybennis.runplanner.client.handlers;
 
+import java.util.List;
+
+import com.anthonybennis.runplanner.client.logic.PlanGenerator;
+import com.anthonybennis.runplanner.client.logic.PlanItem;
+import com.anthonybennis.runplanner.client.logic.UserSettingsValidator;
+import com.anthonybennis.runplanner.client.storage.Persistance;
+import com.anthonybennis.runplanner.client.utils.RunPlannerDate;
 import com.google.gwt.event.dom.client.TouchEndEvent;
 import com.google.gwt.event.dom.client.TouchEndHandler;
 
@@ -18,17 +25,32 @@ public class CreatePlanClickHandler implements TouchEndHandler
 
 	private void createPlan()
 	{
-
-		/*
-		 * Get Distance
-		 */
+		String distance = Persistance.get(Persistance.TARGET_DISTANCE);
+		String date = Persistance.get(Persistance.TARGET_DATE);
+		String experience = Persistance.get(Persistance.EXPERIENCE_LEVEL);
+		
+		UserSettingsValidator validator = new UserSettingsValidator(); 
+		boolean isValid = validator.validateUserSettings(distance, date, experience);
 		
 		/*
-		 * Get Race Date
+		 * Create Plan
 		 */
-		
-		/*
-		 * Get Experience Level
-		 */
+		if (isValid)
+		{
+			RunPlannerDate runPlannerDate = new RunPlannerDate();
+			runPlannerDate.setDay(1);
+			runPlannerDate.setMonth(9);
+			runPlannerDate.setYear(2012);
+			
+			PlanGenerator gen = new PlanGenerator(runPlannerDate);
+			List<PlanItem> plan = gen.generatePlan(5, 0);
+			gen.savePlan(plan);
+			
+			/*
+			 * TODO Create Calender!
+			 */
+		}
 	}
+	
+	
 }
