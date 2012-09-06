06372121c05d0ab1bc6adb5256515b2c292eea72
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2012-09-06 13:47:24 +0000

    Plan is now created using real data

--- trunk/RunPlanner/src/com/anthonybennis/runplanner/client/handlers/CreatePlanClickHandler.java
+++ trunk/RunPlanner/src/com/anthonybennis/runplanner/client/handlers/CreatePlanClickHandler.java
@@ -2,27 +2,51 @@ package com.anthonybennis.runplanner.client.handlers;
 
 import java.util.List;
 
+import com.anthonybennis.runplanner.client.DistancePanelManager;
+import com.anthonybennis.runplanner.client.controls.calendar.CalanderManager;
 import com.anthonybennis.runplanner.client.logic.PlanGenerator;
 import com.anthonybennis.runplanner.client.logic.PlanItem;
 import com.anthonybennis.runplanner.client.logic.UserSettingsValidator;
 import com.anthonybennis.runplanner.client.storage.Persistance;
 import com.anthonybennis.runplanner.client.utils.RunPlannerDate;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.TouchEndEvent;
 import com.google.gwt.event.dom.client.TouchEndHandler;
 
 /**
  * Creates Plan from user selected options
  * @author Anthony
- *
  */
-public class CreatePlanClickHandler implements TouchEndHandler 
+public class CreatePlanClickHandler implements TouchEndHandler, ClickHandler
 {
+	private CalanderManager _calanderManager;
+	
+	/**
+	 * 
+	 */
+	public CreatePlanClickHandler(CalanderManager calanderManager)
+	{
+		_calanderManager = calanderManager;
+	}
+	
 	@Override
 	public void onTouchEnd(TouchEndEvent event) 
 	{
 		this.createPlan();
 	}
+	
+	
 
+	@Override
+	public void onClick(ClickEvent event) 
+	{
+		this.createPlan();	
+	}
+
+	/**
+	 * 
+	 */
 	private void createPlan()
 	{
 		String distance = Persistance.get(Persistance.TARGET_DISTANCE);
@@ -37,18 +61,12 @@ public class CreatePlanClickHandler implements TouchEndHandler
 		 */
 		if (isValid)
 		{
-			RunPlannerDate runPlannerDate = new RunPlannerDate();
-			runPlannerDate.setDay(1);
-			runPlannerDate.setMonth(9);
-			runPlannerDate.setYear(2012);
-			
-			PlanGenerator gen = new PlanGenerator(5, 0,runPlannerDate);
+			DistancePanelManager.DISTANCE convertedDistance = DistancePanelManager.convertPreferenceStringToDistance(distance);
+			RunPlannerDate runPlannerDate = RunPlannerDate.convertStringToDate(date);
+			PlanGenerator gen = new PlanGenerator(convertedDistance, Integer.parseInt(experience),runPlannerDate);
 			List<PlanItem> plan = gen.generatePlan();
 			gen.savePlan(plan);
-			
-			/*
-			 * TODO Create Calender!
-			 */
+			_calanderManager.update(plan);
 		}
 	}
 }
