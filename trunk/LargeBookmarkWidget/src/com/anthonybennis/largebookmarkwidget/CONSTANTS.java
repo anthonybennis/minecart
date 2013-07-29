4f866d13fc38529a92fe5f21d20a126be3d4ca77
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2013-07-29 20:38:46 +0000

    Now supports Chrome Bookmarks

--- trunk/LargeBookmarkWidget/src/com/anthonybennis/largebookmarkwidget/CONSTANTS.java
+++ trunk/LargeBookmarkWidget/src/com/anthonybennis/largebookmarkwidget/CONSTANTS.java
@@ -5,6 +5,6 @@ public interface CONSTANTS
 	public static final String URL = "com.anthonybennis.largebookmarkwidget.URL";
 	public static final String TITLE = "com.anthonybennis.largebookmarkwidget.TITLE";
 	public static final String SHARED_PREFERENCE = "LARGE_BOOKMARK_WIDGET_FILE";
-	public static final int BOOKMARK_URL_INDEX = 0;
-	public static final int BOOKMARK_TITLE_INDEX = 1;
+	public static final int BOOKMARK_URL_INDEX = 1;
+	public static final int BOOKMARK_TITLE_INDEX = 0;
 }
