56d2d8cfe2f48765a4bed436f7b8abe75051c5f8
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2013-12-02 09:27:12 +0000

    Added settings to Settings Fragment.

--- HomeHeatingCostTracker/src/com/anthonybennis/homeheatingcosttracker/SettingsFragment.java
+++ HomeHeatingCostTracker/src/com/anthonybennis/homeheatingcosttracker/SettingsFragment.java
@@ -7,7 +7,7 @@ import android.preference.PreferenceFragment;
  * 
  * @author abennis
  */
-public class SettingsFragment extends PreferenceFragment 
+public class SettingsFragment extends PreferenceFragment // implements OnSharedPreferenceChangeListener
 {
 	@Override
     public void onCreate(Bundle savedInstanceState)
@@ -15,4 +15,24 @@ public class SettingsFragment extends PreferenceFragment
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
     }
+
+//	/**
+//	 * onSharedPreferenceChanged
+//	 */
+//	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) 
+//	{
+//	    Preference pref = findPreference(key);
+//	    
+//	    if (pref instanceof ListPreference)
+//	    {
+//	        ListPreference listPref = (ListPreference) pref;
+//	        pref.setSummary(listPref.getEntry());
+//	    }
+//	    else if (pref instanceof EditTextPreference)
+//	    {
+//	    	EditTextPreference editTextPref = (EditTextPreference)pref;
+//	    	editTextPref.setSummary(editTextPref.getText());
+//	    }
+//	    
+//	}
 }
