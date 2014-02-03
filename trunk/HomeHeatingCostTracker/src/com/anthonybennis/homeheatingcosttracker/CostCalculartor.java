7cbeec901a1974b1c4e88aebed9c8e963672c2ff
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2014-02-03 15:45:03 +0000

    Now using User Preference Values for calculating costs.

--- trunk/HomeHeatingCostTracker/src/com/anthonybennis/homeheatingcosttracker/CostCalculartor.java
+++ trunk/HomeHeatingCostTracker/src/com/anthonybennis/homeheatingcosttracker/CostCalculartor.java
@@ -2,6 +2,8 @@ package com.anthonybennis.homeheatingcosttracker;
 
 import java.text.DecimalFormat;
 
+import android.content.Context;
+
 /**
  * 
  * @author abennis
@@ -13,14 +15,14 @@ public class CostCalculartor
 	 * @param startTime
 	 * @return
 	 */
-	public static String calculateCost(long startTime)
+	public static String calculateCost(long startTime, Context context)
 	{
-		String roundedCostOfOilUsed = "€0.00";
+		String roundedCostOfOilUsed = CostCalculartor.getCurrencySymbol(context) + "0.00";
 		
 		DecimalFormat decimalFormat;
-		double oilBurnTime = CostCalculartor.getOilBurnPercentage();
+		double oilBurnTime = CostCalculartor.getOilBurnPercentage(context);
 		final double oilPerHour100PerCent = 2.27; // Litres. Fixed value.
-		double oilCostPerLitre = CostCalculartor.getOilCostPerLitre();
+		double oilCostPerLitre = CostCalculartor.getOilCostPerLitre(context);
 		double oilUsed =  (oilPerHour100PerCent*oilBurnTime); // For example, €1.20 per hour.
 		double hourlyOilCost = (oilUsed*oilCostPerLitre);
 		double secondOilCost = (hourlyOilCost/3600);
@@ -36,14 +38,14 @@ public class CostCalculartor
 		currentCostOfOilUsed = durationInSeconds*secondOilCost;
 		decimalFormat = new DecimalFormat("##.00");
 		roundedCostOfOilUsed = decimalFormat.format(currentCostOfOilUsed);
-		int indexOfDecimalPoint = roundedCostOfOilUsed.indexOf('.');
+		int indexOfDecimalPoint = roundedCostOfOilUsed.indexOf('.'); // TODO AB Refactor for European use of ','
 			
 		if (indexOfDecimalPoint == 0)
 		{
 			roundedCostOfOilUsed = "0" + roundedCostOfOilUsed;
 		}
 			
-		roundedCostOfOilUsed = "€" + roundedCostOfOilUsed;
+		roundedCostOfOilUsed = CostCalculartor.getCurrencySymbol(context) + roundedCostOfOilUsed;
 		
 		return roundedCostOfOilUsed;
 	}
@@ -52,18 +54,50 @@ public class CostCalculartor
 	 * 
 	 * @return
 	 */
-	private static double getOilCostPerLitre()
+	private static double getOilCostPerLitre(Context context)
 	{
-		double oilCostPerLitre = 0.85; // Default (December 2013 prices for Cork).
-				
-		// TODO AB: Read from settings.
-				
+		double oilCostPerLitre = PreferencesUtil.loadPreferenceAsDoubleg(PreferencesUtil.COST_PER_LITRE, context);
 		return oilCostPerLitre;
 	}
 	
-	private static double getOilBurnPercentage()
+	/**
+	 * 
+	 * @param context
+	 * @return
+	 */
+	private static double getOilBurnPercentage(Context context)
 	{
-		// TODO Get BER rating.
-		return 0.65; // TODO - Increase for bad BERs
+		double burnPercentage = 0.65;	
+		String insulationRating = PreferencesUtil.loadPreference(PreferencesUtil.BER, context);
+		
+		/*
+		 * TODO AB: Refactor 
+		 * remove String literals from here and improve how we refer to array values.
+		 */
+		if (insulationRating.equals("Good Insulation"))
+		{
+			burnPercentage = 0.60; // Burns less often.
+		}
+		else if (insulationRating.equals("Poor Insulation"))
+		{
+			burnPercentage = 0.70; // Burns more often.
+		}
+		else
+		{
+			burnPercentage = 0.65; // Default (Average insulation).
+		}
+		
+		return burnPercentage;
+	}
+	
+	/**
+	 * 
+	 * @param context
+	 * @return
+	 */
+	protected static String getCurrencySymbol(Context context)
+	{
+		String currenceySymbol = PreferencesUtil.loadPreference(PreferencesUtil.CURRENCY_SYMBOL, context);
+		return currenceySymbol;
 	}
 }
