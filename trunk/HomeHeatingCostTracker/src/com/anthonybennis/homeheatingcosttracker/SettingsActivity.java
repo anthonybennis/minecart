221741f28e33e097ce51816ce73f902d57b12acd
anthonybennis@gmail.com <anthonybennis@gmail.com@e3973b4c-657b-8899-d8bd-31a5b08abdc0>
2014-02-03 16:27:24 +0000

    Deleted dead code.

--- trunk/HomeHeatingCostTracker/src/com/anthonybennis/homeheatingcosttracker/SettingsActivity.java
+++ trunk/HomeHeatingCostTracker/src/com/anthonybennis/homeheatingcosttracker/SettingsActivity.java
@@ -17,23 +17,4 @@ public class SettingsActivity extends Activity
 	      this.getActionBar().setDisplayHomeAsUpEnabled(true);
 	      getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
 	 }
-	 
-//	 @Override
-//	 public boolean onOptionsItemSelected(MenuItem item) 
-//	 {
-//		 /*
-//		  * Allows the user to exit the SettingsActivity and Navigate back to
-//		  * home by pressing the app icon on the ActionBar.
-//		  */
-//	     switch (item.getItemId()) 
-//	     {
-//		     // Respond to the action bar's Up/Home button
-//		     case android.R.id.home:
-//		     {
-//		    	 navigateUpToFromChild(this, )
-//		         return true;
-//		     }
-//	     }
-//	     return super.onOptionsItemSelected(item);
-//	 }
 }
